e1327aaff7d25018f202769eb9bf604f7626830b
Marcos Felix Costa <marcosfelixcosta@gmail.com>
2024-08-02 16:16:49 -0300

    Update Aura.Target.cs

--- Source/Aura.Target.cs
+++ Source/Aura.Target.cs
@@ -8,8 +8,8 @@ public class AuraTarget : TargetRules
 {
 	public AuraTarget(TargetInfo Target) : base(Target)
 	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "Aura" } );
+	Type = TargetType.Game;
+	DefaultBuildSettings = BuildSettingsVersion.V2;
+	ExtraModuleNames.AddRange( new string[] { "Aura" } );
 	}
 }
